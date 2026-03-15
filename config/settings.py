b516c7f4946bf0f7220cef6bceb45100fc8b96f4
SruthiGS-Gito <212714937+SruthiGS-Gito@users.noreply.github.com>
2026-03-15 20:47:57 +0530

    Update

--- config/settings.py
+++ config/settings.py
@@ -31,7 +31,12 @@ SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DJANGO_DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = [
+    'ourhome-production-b6b3.up.railway.app', 
+    '127.0.0.1', 
+    'localhost',
+    'daring-creativity-production.up.railway.app' # Adding this as a backup based on your project name
+]
 
 
 # Application definition
